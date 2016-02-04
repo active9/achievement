7e39d27459eccdf14ee562d585c59a3e8da176ac
Chris McCaulley <talkquazi@gmail.com>
2016-02-03 21:38:44 -0500

    initial commit

--- examples/test.js
+++ examples/test.js
@@ -1,24 +1,3 @@
-#Achievement
-![Event Driven Reward System](https://raw.githubusercontent.com/active9/achievement/master/achievement.png)
-
-Achievement is a simple event driven reward system written in NodeJS.
-
-#INSTALLING
-Using Git:
-```bash
-git clone https://github.com/active9/achievement
-cd achievement*
-npm install
-```
-
-Using NPM:
-```bash
-npm install achievement
-```
-
-#MODULE
-Above may run included as a module in your projects.
-```js
 var achievement = require('../');
 
 achievement.rewards.add('New User', function() {
@@ -40,18 +19,3 @@ achievement.emit('New User Again', 'Joe', function(result, user) {
 	// This should not emit again
 	console.log('Achievement Again:', result, user);
 });
-
-```
-
-
-~Enjoy!
-
-#CONTRIB
-
-Achievement is open-source via the MIT license we encourage Forking.
-
-#LICENSE
-MIT
-
-
-
